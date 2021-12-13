c896e2939fb80caa21ab8b4f6507a1d3cbc67f49
25Simone <dantsimone111@gmail.com>
2021-12-13 18:13:24 +0100

    esercizio bonus completato

--- js/script-bonus.js
+++ js/script-bonus.js
@@ -8,10 +8,18 @@ const playerNumber = parseInt(prompt('Inserisci il numero di giocatori'));
 const numList = [];
 
 // definisco un ciclo for per ripetere l'estrazione randomica di numeri per il numero di giocatori scelto dall'utente
-for (let i = 0; i < playerNumber; i++) {
+for (let i = 1; i <= playerNumber; i++) {
     let num = Math.floor(Math.random() * 6) + 1;
     // inserisco i numeri nell'array
     numList.push(num);
+    // numList.push(`player${i}_${num}`);
 }
 
+// definisco la funzione che indica il numero piuù grande dell'array
 let winnerNumber = (Math.max(...numList));
+
+// definisco la funzione che associa al numero vincente il primo giocatore che lo ha estratto
+let winnerPlayer = parseInt(numList.indexOf(winnerNumber));
+
+// stampo a schermo l'esito del gioco
+diceGame.append('Player' + (winnerPlayer + 1));
